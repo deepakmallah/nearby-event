2db6935d38fdcd9d694860eba6c4eaa5fa6169a7
deepakmallah <deepak.mallah@gmail.com>
2017-11-19 20:10:16 +0530

    API for pulling Categories

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,6 +1,29 @@
 'use strict';
 
+const Promise = require("bluebird");
+const request = require("request");
+const token = "KFIMKJ3IIGGSXHEVAULZ";
+
 module.exports = (req, res) => {
 
-  res.render('index.ejs', {msg: "Hello World"});
-}
+  function getCategories() {
+    return new Promise((resolve, reject) => {
+      request(`https://www.eventbriteapi.com/v3/categories?token=${token}`, function (error, response, body) {
+        if(error) resolve(0);
+
+        if (!error && response.statusCode == 200) {
+          resolve(JSON.parse(body));
+        }
+      });
+    })
+  }
+
+  getCategories()
+    .then((resData) => {
+      console.log("getCategories", resData)
+      res.render('index.ejs', {categories: JSON.stringify(resData.categories)});
+    })
+    .catch((err) => {
+      console.log("Error occured", err)
+    });
+};
