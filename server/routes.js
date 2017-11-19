189831f0cd49e95ea7e605fb6f2f4e2ff0d81a65
deepakmallah <deepak.mallah@gmail.com>
2017-11-19 20:10:37 +0530

    Request and Bluebird

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@
 function route(app) {
   // index page
   app.get("/", require('./controllers/indexController'));
+  app.get("/events/:lon/:lat/:radius/:category", require('./controllers/eventController'));
 }
 
 module.exports.route = route;
